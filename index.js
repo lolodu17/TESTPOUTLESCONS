a2fb1cb65cadce6a9f743fa8fce0498563a67047
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2020-11-18 17:46:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const Discord = require("Discord.js")
+const bot = new Discord.client()
 
+bot.on("ready" function() =>{
+  console.log("Coucou")
+})
+
+bot.login("Nzc4NjYxNzQ3NTAyMjE5MjY0.X7VPQw.D12-FUIQCu170qrPfkyAjNywzoI")
