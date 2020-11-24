15b63ed366c3ea8f627ae53a7272ad9a99b6b358
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2020-11-24 13:18:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const Discord = require("Discord.js")
 const bot = new Discord.client()
 
+var prefix = "!";
+
 bot.on("ready", function(){
   console.log("Coucou")
 })
+
+bot.on("message", message =>{
+ if(message.content.startsWith(prefix + "test"){
+    message.reply("Test réussi !")
+    }}
 
 bot.login(PROCESS.env.TOKEN)
